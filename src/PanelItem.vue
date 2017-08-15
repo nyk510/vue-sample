0fed0384257b2c7f08831bc1a96ecbbb75d94677
nyk510 <takamail53@gmail.com>
2017-08-15 18:45:26 +0900

    [Update] 見え方綺麗に

--- src/PanelItem.vue
+++ src/PanelItem.vue
@@ -3,35 +3,43 @@
         <edit-event :is-show="isShowEdit" @ok="update" header="編集モード" :default="item" @cancel="isShowEdit = false">
         </edit-event>
         <div class="panel panel-default">
-            <div class="panel-heading">
+            <div class="panel-body">
                 <h3>
                     {{ item.title }}
                 </h3>
                 <div>
+                    Assigned:
                     <span class="label label-default" v-for="(person, key) in item.assignedPersons" :key="key">
                         {{ person.name }}
                     </span>
                 </div>
-            </div>
-            <div class="panel-body">
                 <div>
-                    <p>
-                        {{ item.contents }}
-                    </p>
-                    <p>
-                        責任者: {{ item.responsiblePerson.name }}
-                    </p>
                     <section>
-                        <h4>締め切り</h4>
-                        <p> {{ item.deadlineDate }} </p>
+                        <h4>内容</h4>
+                        <p>
+                            {{ item.contents }}
+                        </p>
+                    </section>
+                    <section>
+                        <h4>締め切り： {{ item.deadlineDate }}
+                            <span class="label label-danger" v-if="isOver"> Overed </span>
+                        </h4>
                     </section>
                 </div>
             </div>
             <div class="panel-footer">
                 <button class="btn btn-default" @click="isShowEdit = true">編集</button>
-                <button class="btn btn-default" @click="onDelete">削除</button>
+                <button class="btn btn-default" @click="isShowDelete = true">削除</button>
             </div>
         </div>
+        <modal v-model="isShowDelete" @ok="onDelete" title="削除" cancel-text="キャンセル" ok-text="削除">
+            以下のデータを削除します
+            <ul>
+                <li>Title: {{ item.title }}</li>
+                <li>内容: {{ item.contents }}</li>
+                <li>締め切り: {{ item.deadlineDate }}</li>
+            </ul>
+        </modal>
     </div>
 </template>
 
@@ -39,20 +47,20 @@
 import Vue from "vue"
 import Component from "vue-class-component"
 import EditEvent from './EditEvent.vue'
+import { modal } from 'vue-strap'
 import { TodoItem } from './Model'
 
 @Component({
     components: {
-        EditEvent
+        EditEvent, modal
     },
     props: {
-        item: {
-
-        }
+        item: {}
     }
 })
 export default class PanelItem extends Vue {
     isShowEdit: boolean = false
+    isShowDelete: boolean = false
     item: TodoItem
     nowDate = new Date()
 
@@ -63,6 +71,16 @@ export default class PanelItem extends Vue {
 
     onDelete() {
         this.$emit("delete", this.item)
+        this.isShowDelete = false
+    }
+
+    get isOver() {
+        let d = new Date(this.item.deadlineDate)
+        if (d.getTime() > this.nowDate.getTime()) {
+            return false
+        } else {
+            return true
+        }
     }
 }
 </script>
