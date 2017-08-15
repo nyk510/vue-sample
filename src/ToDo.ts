b20a614f12beb181462d3f09548d7ba3e0014655
nyk510 <takamail53@gmail.com>
2017-08-15 18:45:51 +0900

    [Add] 最初から何も入ってないのかなしいので初期値追加

--- src/ToDo.ts
+++ src/ToDo.ts
@@ -1,6 +1,21 @@
 import ItemList from './ItemList.vue'
 import Vue from "vue"
+import { TodoItem, TodoObject, Manager } from './Model'
+let persons = Manager.persons
+
+let todoList = []
+let v: TodoObject = {
+    title: "将棋",
+    contents: "詰将棋の本を10ページ読む",
+    assignedPersons: [],
+    responsiblePerson: persons[0],
+    deadlineDate: "2017/11/22"
+}
+todoList.push(new TodoItem(v))
 
 let main = new ItemList({
-    el: "#main"
+    el: "#main",
+    data: {
+        todoItems: todoList
+    }
 })
